f67fca843495fff8dc2bf7d05d1d87a630b72169
yty0643 <80657819+yty0643@users.noreply.github.com>
2022-01-14 13:22:42 +0900

    add editor & preview component

---
+++ src/components/preview/preview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./preview.module.css";
+
+const Preview = (props) => {
+  return (
+    <section className={styles.preview}>
+      <h1 className={styles.title}>preview</h1>;
+    </section>
+  );
+};
+export default Preview;
