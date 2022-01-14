eb9c19bdf216369cc35d7e28af7305ad663b2544
yty0643 <80657819+yty0643@users.noreply.github.com>
2022-01-14 17:11:29 +0900

    add card & edit form component

---
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Button from "../button/button";
+import ImageFileInput from "../image_file_input/image_file_input";
+import styles from "./card_edit_form.module.css";
+const CardEditForm = ({ card }) => {
+  const {
+    //
+    name,
+    company,
+    title,
+    email,
+    message,
+    theme,
+    fileName,
+    fileURL,
+  } = card;
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form className={styles.form}>
+      <input
+        className={styles.input}
+        type="text"
+        name="name"
+        defaultValue={name}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="company"
+        defaultValue={company}
+      />
+      <select className={styles.select} name="theme" defaultValue={theme}>
+        <option value="light"></option>
+        <option value="dark"></option>
+        <option value="colorful"></option>
+      </select>
+      <input
+        className={styles.input}
+        type="text"
+        name="title"
+        defaultValue={title}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="email"
+        defaultValue={email}
+      />
+      <textarea
+        className={styles.textarea}
+        name="message"
+        defaultValue={message}
+      />
+      <div className={styles.fileInput}>
+        <ImageFileInput />
+      </div>
+      <Button name="Delete" onClick={onSubmit} />
+    </form>
+  );
+};
+export default CardEditForm;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,11 +1,47 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react/cjs/react.development";
 import Editor from "../editor/editor";
 import Footer from "../footer/footer";
 import Header from "../header/header";
 import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "yty",
+      company: "samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "yty0643@naver.com",
+      message: "go for it",
+      fileName: "taeyoung",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "yty2",
+      company: "samsung",
+      theme: "dark",
+      title: "Software Engineer",
+      email: "yty0643@naver.com",
+      message: "go for it",
+      fileName: "taeyoung",
+      fileURL: null,
+    },
+    {
+      id: "3",
+      name: "yty3",
+      company: "samsung",
+      theme: "colorful",
+      title: "Software Engineer",
+      email: "yty0643@naver.com",
+      message: "go for it",
+      fileName: "taeyoung",
+      fileURL: null,
+    },
+  ]);
   const navigate = useNavigate();
   const onLogout = () => {
     authService.logout();
@@ -22,8 +58,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
